8224e96d91bec27a2918cf9931e801b866c41823
Hashem Sowande Gray <hgray1109@gmail.com>
2022-10-07 21:17:44 +0000

    hosting changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBar"
 import Hero from "./Hero"
 import Card from "./Card"
 import data from "./data"
-import Footer from "./footer"
+import Footer from "./Footer"
 
 export default function App() {
   
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Hero() {
     return (
         <div className="hero">
-            <img src="public/images/profile-pic.jpg" className="hero-photo" />
+            <img src={"./images/profile-pic.jpg"} className="hero-photo" />
             <div className="hero-title">
                  <h1 className="hero-text">I am Hashem Sowande Gray. I am a front-end developer!</h1>
                 <p className="hero-subtext">These are some of my favorite places in the world!</p>
